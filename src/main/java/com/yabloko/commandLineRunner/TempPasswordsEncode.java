351b0c5d22cc1266434568568da0b321f7b94c20
Artem Yablokov <apple@rambler.ru>
2020-10-04 13:15:20 +0300

    rest_v_2 password encoder

--- src/main/java/com/yabloko/commandLineRunner/TempPasswordsEncode.java
+++ src/main/java/com/yabloko/commandLineRunner/TempPasswordsEncode.java
@@ -1,14 +1,16 @@
-package com.yabloko;
+package com.yabloko.commandLineRunner;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+
+// не использовался
 @Component
 public class TempPasswordsEncode {
 
     // НЕЛЬЗЯ ПРОСТО ВЗЯТЬ И В ЛЮБОМ МЕТОДЕ ПОЛУЧИТЬ ПРИВЗКУ СПРИНГА
-    // НУЖНО ИСПОЛЬЗОВАТЬ ЗАПУСК ПРИЛОЖЕНИЯ
+    // НУЖНО ИСПОЛЬЗОВАТЬ ЗАПУСК ПРИЛОЖЕНИЯ - для построения контекста
     @Autowired
     PasswordEncoder passwordEncoder;
 
